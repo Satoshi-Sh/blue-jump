b42b28f0bdc911c456628afaac1f4fdfa7521793
satoshi-sh <satoss1108@gmail.com>
2023-12-26 21:40:17 -0600

    Start over with canvas and tutorial

--- src/lib/components/Window.svelte
+++ src/lib/components/Window.svelte
@@ -1,22 +1,3 @@
-<script>
-	import Object from './Object.svelte';
-</script>
+<canvas id="board"></canvas>
 
-<div class="container">
-	<div class="window">
-		<Object />
-	</div>
-</div>
-
-<style>
-	.container {
-		padding: 60px;
-	}
-	.window {
-		margin: auto;
-		position: relative;
-		width: 700px;
-		height: 800px;
-		background-color: lightcyan;
-	}
-</style>
+<style></style>
