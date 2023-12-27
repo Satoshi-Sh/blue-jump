6fc0e2831c44afb659bd0bc43d62ea2f8f61aa5c
satoshi-sh <satoss1108@gmail.com>
2023-12-27 07:56:36 -0600

    created detect collision

--- src/lib/components/Window.svelte
+++ src/lib/components/Window.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { getContext, onMount } from 'svelte';
 	import blueFace from '$lib/images/blueFace.png';
+	import platform from '$lib/images/platform.png';
 	let canvasElement: HTMLCanvasElement;
 	let boardWidth = 700;
 	let boardHeight = 900;
@@ -15,12 +16,20 @@
 
 	//physics
 	let velocityX = 0;
+	let velocityY = 0;
+	let initialVelocityY = -12;
+	let gravity = 0.4;
+	//platforms;
+	let platformArray = [];
+	let platformWidth = 120;
+	let platformHeight = 25;
+	let platformImg;
 
 	const handleKey = (event) => {
-		if (event.key === 'ArrowLeft') {
+		if (event.key === 'ArrowLeft' && velocityX != -4) {
 			velocityX -= 4;
 		}
-		if (event.key === 'ArrowRight') {
+		if (event.key === 'ArrowRight' && velocityX != 4) {
 			velocityX += 4;
 		}
 	};
@@ -36,14 +45,19 @@
 		ctx = canvasElement.getContext('2d');
 		canvasElement.width = boardWidth;
 		canvasElement.height = boardHeight;
-		// ctx.fillStyle = 'green';
-		// ctx.fillRect(doodler.x, doodler.y, doodler.width, doodler.height);
 
 		doodlerImg = new Image();
 		doodlerImg.src = blueFace;
 		doodlerImg.onload = () => {
 			ctx.drawImage(doodlerImg, doodler.x, doodler.y, doodler.width, doodler.height);
 		};
+
+		platformImg = new Image();
+		platformImg.src = platform;
+
+		velocityY = initialVelocityY;
+		placePlatforms();
+
 		requestAnimationFrame(update);
 	});
 	const update = () => {
@@ -52,11 +66,46 @@
 		doodler.x += velocityX;
 		if (doodler.x > boardWidth) {
 			doodler.x = 0;
-		}
-		if (doodler.x + doodler.width < 0) {
+		} else if (doodler.x + doodler.width < 0) {
 			doodler.x = boardWidth;
 		}
+		velocityY += gravity;
+		doodler.y += velocityY;
 		ctx.drawImage(doodlerImg, doodler.x, doodler.y, doodler.width, doodler.height);
+
+		for (let i = 0; i < platformArray.length; i++) {
+			let platform = platformArray[i];
+			if (detectCollision(doodler, platform)) {
+				velocityY = initialVelocityY;
+			}
+			ctx.drawImage(platformImg, platform.x, platform.y, platform.width, platform.height);
+		}
+	};
+
+	const placePlatforms = () => {
+		platformArray = [];
+		let platform = {
+			img: platformImg,
+			x: boardWidth / 2,
+			y: boardHeight - 90,
+			width: platformWidth,
+			height: platformHeight
+		};
+		platformArray.push(platform);
+		platform = {
+			img: platformImg,
+			x: boardWidth / 2,
+			y: boardHeight - 240,
+			width: platformWidth,
+			height: platformHeight
+		};
+		platformArray.push(platform);
+	};
+
+	const detectCollision = (a, b) => {
+		return (
+			a.x < b.x + b.width && a.x + a.width > b.x && a.y < b.y + b.height && a.y + a.height > b.y
+		);
 	};
 </script>
 
